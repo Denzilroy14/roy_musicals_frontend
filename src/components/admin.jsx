4b418b484a6dad312a7c9e2f4a0159902d23213d
Denzil Roy.I <145376164+Denzilroy14@users.noreply.github.com>
2025-09-06 19:09:28 +0530

    Update admin.jsx

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -4,7 +4,7 @@ export default function Admin(){
     const [users,setUsers]=useState([]);
 
     useEffect(()=>{
-        axios.get('http://roymusicalsbackend.pythonanywhere.com/admin')
+        axios.get('https://roymusicalsbackend.pythonanywhere.com/admin')
         .then(res=>setUsers(res.data))
         .catch(err=>console.error(err));
     },[]);
